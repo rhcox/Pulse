a7f6ab905980a525a39ed13806aa155d57891746
rhcox <40273831+rhcox@users.noreply.github.com>
2018-07-09 01:40:50 -0500

    experimental code to calculate pulse and temp

--- Zombie.py
+++ Zombie.py
@@ -6,21 +6,138 @@ from analogio import AnalogIn
 from simpleio import map_range
 import digitalio
 
+def fileerror(text,e):
+    print(text,' ',e)
+    valuel=(255,0,0)
+    if e.args[0] == 30:
+        valuel=(0,0,255)
+    if e.args[0] == 28:
+        valuel=(0,255,0)
+    pixels.fill((0, 0, 0))
+    pixels[9] = valuel
+    pixels.show()
+
+    
+def getvalue(line,n):
+    string=''
+    comma=0
+    for c in line:
+        if(comma<n):
+            if c==',':
+                comma=comma+1
+        else:
+            if c==',':
+                break
+            else:
+                string=string+c
+    return float(string)
+    
+def gettime(line):
+    return getvalue(line,0)
+    
+def gettemp(line):
+    return getvalue(line,1)
+    
+def getsm(line):
+    return getvalue(line,1)
+
+def getpulse(line):
+    return getvalue(line,2)
+    
+def smooth(fp):
+    count=0
+    ftime=[0,0,0,0,0]
+    fpulse=[0,0,0,0,0]
+    try:
+        with open("zombiesm.csv", "w+",0) as fp1:
+            for line in fp:
+                if count>0:
+                    ftime[count-1]=gettime(line)
+                    fpulse[count-1]=getpulse(line)
+                    if(count>4):
+                        total=0
+                        for i in fpulse:
+                            total=total+i
+                        fp1.write('{0:f},'.format(ftime[2]))
+                        fp1.write('{0:f}\n'.format(total/5))
+                        for i in range(0,4):
+                            ftime[i]=ftime[i+1]
+                            fpulse[i]=fpulse[i+1]
+                        count=5
+                    else:
+                        count=count+1
+                else:
+                    count=count+1
+    except OSError as e:
+        fileerror('Zombie Smooth',e)
+
+def pulseRate():
+    pulse=0
+    try:
+        with open("zombiesm.csv", "r",0) as fp:
+            line=fp.readline()
+            smmin=getsm(line)
+            smmax=smmin
+            for line in fp:
+                val=getsm(line)
+                if(val<smmin):
+                    smmin=val
+                if(val>smmax):
+                    smmax=val
+        with open("zombiesm.csv", "r",0) as fp:
+            cutoff=smmax-smmin
+            cutoff=smmin+int(float(cutoff)*0.25)
+            pulse=0
+            flag=True
+            for line in fp:
+                val=getsm(line)
+                if val<cutoff:
+                    if flag:
+                        pulse=pulse+1
+                    flag=False
+                else:
+                    flag=True
+    except OSError as e:
+        fileerror('Zombie Smooth Read',e)
+    return pulse
+
+
+def bodyTemp():
+    try:
+        with open("zombie.csv", "r",0) as fp:
+            line=fp.readline()
+            line=fp.readline()            
+            smmin=gettemp(line)
+            smmax=smmin
+            sumTemp=smmin
+            count=1
+            for line in fp:
+                val=gettemp(line)
+                sumTemp=sumTemp+val
+                if(val<smmin):
+                    smmin=val
+                if(val>smmax):
+                    smmax=val
+                count=count+1
+            if (smmax+smmin)/2 < (sumTemp/count):
+                return False
+    except OSError as e:
+        fileerror('Zombie Body Temperature',e)
+    return True
+
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2)
 
 thermistor = adafruit_thermistor.Thermistor(board.TEMPERATURE, 10000, 10000, 25, 3950)
 
-pixels.fill((100,0,0))
-pixels.show()
 
 analogin = AnalogIn(board.LIGHT)
 
-WaitTime=10.0
+WaitTime=5.0
 DataTime=60.0
 deltat=0.05
 pixelTime=int(float(DataTime-WaitTime)/5.0+.5)
 try:
-    with open("zombie.cvs", "w+",0) as fp:
+    with open("zombie.csv", "w+",0) as fp:
         fp.write('Time, Temperature, Pulse Wave\n')
         fp.flush()
         pixels.fill((0, 100, 0))
@@ -33,7 +150,6 @@ try:
             timew=j*deltat
         pixels.fill((0, 0, 0))
         pixels.show()
-        print('END Wait')
         
         pixels[1] = (0,255,0)
         pixels.show()
@@ -43,7 +159,6 @@ try:
         timed=0
         j=0
         while timed<DataTime:
-#            print('Collect Data= ',j,timed,DataTime)
             temp = thermistor.temperature
             intensity=float(analogin.value)
             #it takes a long time to write
@@ -54,7 +169,7 @@ try:
             time.sleep(deltat)
             j=j+1
             #so add that time adding like this is bad
-            timed=timew+j*(deltat+0.15)
+            timed=timew+j*(deltat+0.40)
             if (timed-pixelStart)>pixelTime:
                 if pixelON>9:
                     pixelON=9
@@ -65,18 +180,42 @@ try:
         pixels.fill((0, 0, 0))
         pixels.show()
         fp.flush()
-
 except OSError as e:
-    print(e)
-    delay = 0.5
-    valuel=(255,0,0)
-    if e.args[0] == 30:
-        valuel=(0,0,255)
-    if e.args[0] == 28:
-        valuel=(0,255,0)
-    pixels.fill((0, 0, 0))
-    pixels[9] = valuel
-    pixels.show()
+    fileerror('Zombie Write',e)
+    
 
-pixels[5] = (50,0,0)
+pixels[4] = ((20, 0, 0))
 pixels.show()
+
+try:
+    with open("zombie.csv", "r",0) as fp:
+        smooth(fp)
+except OSError as e:
+    fileerror('Zombie Read',e)
+
+pixels[3] = ((20, 0, 0))
+pixels.show()
+
+pulseR=pulseRate()
+pixels[2] = ((20, 0, 0))
+pixels.show()
+
+pulseR=int(float(pulseR)/(float(DataTime-WaitTime)/60.0))
+
+pixels.fill((0, 0, 0))
+pixels.show()
+pixelc=pulseR/3
+if pixelc>9:
+    pixelc=9
+    
+if bodyTemp():
+    pixelc=3
+    
+for i in range(0,pixelc):
+    if i<5:
+        pixels[i] = ((0,0,100))
+    elif i<8:
+        pixels[i] = ((0,100,0))
+    else:
+        pixels[i] = ((100,0,0))
+
